364521438df90deb3f244d56a32f3b4ff5735f87
zeroLinWang <364385155@qq.com>
2018-05-31 10:30:17 +0800

    spingboot init

---
+++ src/main/java/com/zero/jimu/JimuApplication.java
@@ -0,0 +1,12 @@
+package com.zero.jimu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimuApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zero/jimu/JimuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zero.jimu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JimuApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
