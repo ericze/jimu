9eb655e1dd5492be1bb48b6e6ac9c5fd383ef540
zeroLinWang <364385155@qq.com>
2018-06-11 17:49:03 +0800

    运用service层

--- src/main/java/com/zero/jimu/controller/UserController.java
+++ src/main/java/com/zero/jimu/controller/UserController.java
@@ -9,14 +9,15 @@ import com.zero.jimu.dao.UserDao;
 import com.zero.jimu.entity.User;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zero.jimu.service.user.UserService;
+
 @RestController
 public class UserController {
     @Autowired
-    UserDao userDao;
+    private UserService userService;
 
     @RequestMapping("/userList")
     public List<User> listUser() throws Exception{
-        List<User> users = userDao.findAll();
-        return users;
+        return userService.findAllUser();
     }
 }
